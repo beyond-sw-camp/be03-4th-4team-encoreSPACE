1f5409df85b1ffa6fde72c0e07a0efe20abc22be
cherishjh <139551676+cherishjh@users.noreply.github.com>
2024-03-12 15:06:28 +0900

    Initial commit

---
+++ Space_Backend/src/main/java/com/encore/space/common/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.encore.space.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public static CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("https://spacetemp88.shop", "http://spacetemp88.shop", "http://www.spacetemp88.shop", "https://www.spacetemp88.shop"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setExposedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+}
