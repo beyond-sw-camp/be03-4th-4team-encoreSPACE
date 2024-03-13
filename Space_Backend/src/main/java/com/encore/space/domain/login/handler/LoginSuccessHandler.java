ea4ea4ba42428a7fbff425703a128ccf0936f4b6
GyeongNam <jo_gn97@naver.com>
2024-03-13 10:52:05 +0900

    Oauth login 확인 6 - 5

--- Space_Backend/src/main/java/com/encore/space/domain/login/handler/LoginSuccessHandler.java
+++ Space_Backend/src/main/java/com/encore/space/domain/login/handler/LoginSuccessHandler.java
@@ -60,7 +60,7 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 
         cookie.setPath("/"); // 모든 경로에 대해 쿠키 전송
         cookie.setSecure(false);
-        cookie.setDomain(".www.spaceencore.shop");
+        cookie.setDomain(".spaceencore.shop");
 //        cookie.setSameSite("None");
 //        cookie.setDomain(".www.spaceencore.shop");
 //        response.addCookie(cookie);
